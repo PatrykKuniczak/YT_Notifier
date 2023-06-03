706e93bffb13ec384d73b4fb141db0734861b294
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2023-06-03 23:03:47 +0200

    chore: Create videoContent component

---
+++ react-app/src/components/ui/makro/video-content.tsx
@@ -0,0 +1,19 @@
+import { StyledThumbnail } from '../atomic/thumbnail.ts';
+import { Stack } from '@mui/system';
+import thumbinail from '../../../assets/thumbnail.png';
+import { StyledAuthorInfo } from '../micro/author-info.tsx';
+import { StyledVideoInfos } from '../micro/video-infos.tsx';
+
+export const StyledVideoContent = () => {
+    return (
+        <Stack
+            direction={'column'}
+            justifyContent={'center'}
+            spacing={1}
+            sx={{ p: 1, borderRadius: 1, bgcolor: 'color.secondary' }}>
+            <StyledThumbnail src={thumbinail} />
+            <StyledAuthorInfo />
+            <StyledVideoInfos />
+        </Stack>
+    );
+};
