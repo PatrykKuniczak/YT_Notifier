ff92c13b9a0c226fc241e44e9d154f2ffee990e8
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2023-06-03 23:04:43 +0200

    chore: Rename App -> app

---
+++ react-app/src/app.tsx
@@ -0,0 +1,37 @@
+import { useTernaryDarkMode } from 'usehooks-ts';
+import GlobalStyles from './data/global-styles.ts';
+import lightTheme from './data/themes/light-theme.ts';
+import darkTheme from './data/themes/dark-theme.ts';
+import { useEffect } from 'react';
+import { ThemeProvider } from '@mui/system';
+import { StyledTitleHeader } from './components/ui/atomic/title-header.ts';
+import { StyledVideoContent } from './components/ui/makro/video-content.tsx';
+import { StyledMainContainer } from './layouts/main-container.tsx';
+
+// FOR DEVELOPMENT TIME
+// const Dev = styled('div')({
+//     padding: 50
+// });
+
+function App() {
+    const { isDarkMode, setTernaryDarkMode } = useTernaryDarkMode();
+
+    useEffect(() => {
+        setTernaryDarkMode('light');
+    }, [setTernaryDarkMode]);
+
+    return (
+        <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+            <GlobalStyles />
+            {/*<Dev>*/}
+            <StyledMainContainer>
+                <StyledTitleHeader> Znalezione Wideo </StyledTitleHeader>
+
+                <StyledVideoContent />
+            </StyledMainContainer>
+            {/*</Dev>*/}
+        </ThemeProvider>
+    );
+}
+
+export default App;
