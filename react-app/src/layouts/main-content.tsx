ab466df2f0a91a40a60c96cf648bd03206555ea6
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2023-06-04 15:23:50 +0200

    chore: Create main-content component

---
+++ react-app/src/layouts/main-content.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from 'react';
+import { Stack } from '@mui/system';
+
+export const StyledMainContent = ({ children }: { children: ReactNode }) => {
+    return (
+        <Stack
+            component="main"
+            alignItems={'center'}
+            spacing={1}
+            useFlexGap={true}
+            sx={{
+                position: 'relative',
+                width: 300,
+                height: 600,
+
+                p: 2,
+                bgcolor: 'background.primary',
+                borderRadius: 3
+            }}>
+            {children}
+        </Stack>
+    );
+};
