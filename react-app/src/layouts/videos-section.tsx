d1f54d61bf34be7cbf6489525613e4e9412514fb
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2023-06-07 22:05:32 +0200

    chore: Create scroll-bar for videos-section

--- react-app/src/layouts/videos-section.tsx
+++ react-app/src/layouts/videos-section.tsx
@@ -4,16 +4,40 @@ import { ReactNode } from 'react';
 export const StyledVideosSection = ({ children }: { children: ReactNode }) => {
     return (
         <Stack
-            component="section"
+            component={'section'}
             alignItems={'center'}
+            useFlexGap={true}
             spacing={1}
             sx={{
                 overflow: 'auto',
 
-                bgcolor: 'background.primary'
+                bgcolor: 'background.primary',
+
+                '&::-webkit-scrollbar': {
+                    width: 12
+                },
+
+                '&::-webkit-scrollbar-track': {
+                    my: 1
+                },
+
+                '&::-webkit-scrollbar-thumb': {
+                    border: '2px solid transparent',
+                    borderRadius: 2,
+
+                    bgcolor: '#7846F0',
+                    backgroundClip: 'padding-box'
+                },
+
+                '&::-webkit-scrollbar-thumb:hover': {
+                    bgcolor: '#6833fc'
+                },
+
+                '&::-webkit-scrollbar-thumb:active': {
+                    bgcolor: '#581fe1'
+                }
             }}>
             {children}
         </Stack>
     );
 };
-export default {};
