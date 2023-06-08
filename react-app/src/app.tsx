a203402033701ea04b0fe5788176366ae3afcdac
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2023-06-08 10:30:37 +0200

    chore: Add search bar for main view

--- react-app/src/app.tsx
+++ react-app/src/app.tsx
@@ -8,6 +8,7 @@ import { StyledTitleHeader } from './components/ui/atomic/title-header.ts';
 import { StyledMainContent } from './layouts/main-content.tsx';
 import { StyledVideoArticle } from './components/ui/makro/video-article.tsx';
 import { StyledVideosSection } from './layouts/videos-section.tsx';
+import { StyledSearchBar } from './components/ui/atomic/search-bar.tsx';
 
 function App() {
     const { isDarkMode, setTernaryDarkMode } = useTernaryDarkMode();
@@ -21,6 +22,8 @@ function App() {
             <GlobalStyles />
             <StyledMainContent>
                 <StyledTitleHeader> Znalezione Wideo </StyledTitleHeader>
+
+                <StyledSearchBar />
 
                 <StyledVideosSection>
                     <StyledVideoArticle />
