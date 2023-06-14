9f291e9625cb88e3ff6c0c1565061c439278abd2
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2023-06-14 15:13:39 +0200

    clean: Move search-bar-wrapper.ts to proper dir

--- react-app/src/components/functional/makro/searchBar/search-bar.tsx
+++ react-app/src/components/functional/makro/searchBar/search-bar.tsx
@@ -1,5 +1,5 @@
 import { StyledIcon } from '../../../ui/atomic/icon.ts';
-import { StyledSearchBarWrapper } from '../../atomic/search-bar-wrapper.ts';
+import { StyledSearchBarWrapper } from '../../../ui/atomic/search-bar-wrapper.ts';
 import searchIcon from '../../../../assets/search-icon.svg';
 import { StyledSearchBarInput } from '../../atomic/search-bar-input.tsx';
 import { Ref } from 'react';
