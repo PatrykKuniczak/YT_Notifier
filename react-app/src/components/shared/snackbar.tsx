8a1fb6b916177c3a1c316f8fce541eb44116f365
masterHAWK99 <maciej.pochron99@gmail.com>
2023-07-09 19:15:10 +0200

    Simplify and reformat code

--- react-app/src/components/shared/snackbar.tsx
+++ react-app/src/components/shared/snackbar.tsx
@@ -1,34 +1,38 @@
-import {css, keyframes, styled} from "@mui/system";
+import { css, keyframes, styled } from '@mui/system';
 import useSnackbar from '@mui/base/useSnackbar';
-import {useEffect, useState} from "react";
+import { useEffect, useState } from 'react';
 
-const Snackbar = ({opened, content}: { opened: boolean, content: string }) => {
+const Snackbar = ({
+    opened,
+    content
+}: {
+    opened: boolean;
+    content: string;
+}) => {
     const [open, setOpen] = useState(false);
 
     const handleClose = () => {
         setOpen(false);
     };
 
-    const {getRootProps,} = useSnackbar({
+    const { getRootProps } = useSnackbar({
         onClose: handleClose,
         open,
-        autoHideDuration: 5000,
+        autoHideDuration: 5000
     });
 
     useEffect(() => {
-        if (opened) {
-            setOpen(true);
-        }
+        opened && setOpen(true);
     }, [opened]);
 
     return (
         <>
-            {open ? (
+            {open && (
                 <CustomSnackbar {...getRootProps()}>{content}</CustomSnackbar>
-            ) : null}
+            )}
         </>
     );
-}
+};
 
 const grey = {
     50: '#f6f8fa',
@@ -40,7 +44,7 @@ const grey = {
     600: '#57606a',
     700: '#424a53',
     800: '#32383f',
-    900: '#24292f',
+    900: '#24292f'
 };
 
 const snackbarInRight = keyframes`
@@ -54,29 +58,32 @@ const snackbarInRight = keyframes`
 `;
 
 const CustomSnackbar = styled('div')(
-    ({theme}) => css`
-      position: fixed;
-      z-index: 5500;
-      display: flex;
-      right: 16px;
-      bottom: 16px;
-      left: auto;
-      justify-content: start;
-      max-width: 560px;
-      min-width: 300px;
-      background-color: ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
-      border-radius: 8px;
-      border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
-      box-shadow: ${theme.palette.mode === 'dark'
-              ? `0 2px 8px rgb(0 0 0 / 0.5)`
-              : `0 2px 8px ${grey[200]}`};
-      padding: 0.75rem;
-      color: ${theme.palette.mode === 'dark' ? grey[50] : grey[900]};
-      font-family: 'IBM Plex Sans', sans-serif;
-      font-weight: 600;
-      animation: ${snackbarInRight} 200ms;
-      transition: transform 0.2s ease-out;
-    `,
+    ({ theme }) => css`
+        position: fixed;
+        z-index: 5500;
+        display: flex;
+        right: 16px;
+        bottom: 16px;
+        left: auto;
+        justify-content: start;
+        max-width: 560px;
+        min-width: 300px;
+        background-color: ${theme.palette.mode === 'dark'
+            ? grey[900]
+            : grey[50]};
+        border-radius: 8px;
+        border: 1px solid
+            ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
+        box-shadow: ${theme.palette.mode === 'dark'
+            ? `0 2px 8px rgb(0 0 0 / 0.5)`
+            : `0 2px 8px ${grey[200]}`};
+        padding: 0.75rem;
+        color: ${theme.palette.mode === 'dark' ? grey[50] : grey[900]};
+        font-family: 'IBM Plex Sans', sans-serif;
+        font-weight: 600;
+        animation: ${snackbarInRight} 200ms;
+        transition: transform 0.2s ease-out;
+    `
 );
 
 export default Snackbar;
