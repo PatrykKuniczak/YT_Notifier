d858fa341c1c09b97fdb79f431659ca26086fa11
masterHAWK99 <maciej.pochron99@gmail.com>
2023-08-12 20:11:21 +0200

    Update mui dependencies

--- react-app/src/components/shared/button.ts
+++ react-app/src/components/shared/button.ts
@@ -1,5 +1,5 @@
 import { styled } from '@mui/system';
-import Button from '@mui/base/Button';
+import { Button } from '@mui/base/Button';
 
 export const StyledButton = styled(Button)(({ theme }) =>
     theme.unstable_sx({
