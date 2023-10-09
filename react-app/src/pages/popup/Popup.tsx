c82e8c16ae97f27efbc76852a2a1db2d82f29c77
Patryk Kuniczak <p.kuniczak@gmail.com>
2023-10-09 22:01:38 +0200

    improvement: create wrapper for providers

--- react-app/src/pages/popup/Popup.tsx
+++ react-app/src/pages/popup/Popup.tsx
@@ -1,21 +1,17 @@
 import React, { useMemo } from "react";
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import AuthPage from "@pages/popup/pages/auth/auth.page";
 import HomePage from "@pages/popup/pages/home/home.page";
 import { VideosRoute } from "@pages/popup/routes/videos.route";
 import { StoreRoute } from "@pages/popup/routes/store.route";
-import GlobalStyles from "@pages/popup/data/global-styles";
-import { ThemeProvider } from "@mui/system";
-import darkTheme from "@pages/popup/data/themes/dark-theme";
-import lightTheme from "@pages/popup/data/themes/light-theme";
 import { useTernaryDarkMode } from "usehooks-ts";
 import { IUser } from "@root/utils/interfaces/user.interface";
 import urls from "@root/utils/endpoints/urls";
-import { AuthContext } from "@root/utils/core/authentication/authentication";
 import { useQuery } from "@tanstack/react-query";
 import withErrorBoundary from "@root/utils/hooks/with-error-boundary";
 import withSuspense from "@root/utils/hooks/with-suspense";
 import httpClient from "@root/utils/core/http-client/httpClient";
+import ProvidersWrapper from "@src/providers-wrapper";
 
 const hashRouting = createHashRouter([
     {
@@ -53,17 +49,7 @@ const Popup = () => {
     user
   }), [user]);
 
-  return (
-    <React.StrictMode>
-      <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
-        <GlobalStyles />
-
-        <AuthContext.Provider value={providerValues}>
-          <RouterProvider router={hashRouting} />
-        </AuthContext.Provider>
-      </ThemeProvider>
-    </React.StrictMode>
-  );
+  return (<ProvidersWrapper isDarkMode={isDarkMode} authProviderValues={providerValues} hashRouting={hashRouting} />);
 };
 
 export default withErrorBoundary(withSuspense(Popup, <div> Loading </div>), <div> Error </div>);
