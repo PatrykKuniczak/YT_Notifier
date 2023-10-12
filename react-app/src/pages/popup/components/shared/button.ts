0fb078a338c103f04c317ee01f769ca300a31c02
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2023-10-12 20:05:14 +0200

    Feat/create navbar styles #91 (#99)
    * fix: import of TVoid type (#97)
    
    * Update button.ts
    
    * style: box shadow for currently used navbar option
    
    * style: restructure styles hierarchy
    
    ---------
    
    Co-authored-by: Bartosz Wysowski <bartoszwysowski@gmail.com>

--- react-app/src/pages/popup/components/shared/button.ts
+++ react-app/src/pages/popup/components/shared/button.ts
@@ -1,10 +1,9 @@
-import { styled } from '@mui/system';
-import { Button } from '@mui/base/Button';
+import { styled } from "@mui/system";
+import { Button } from "@mui/base/Button";
 
 export const StyledButton = styled(Button)(({ theme }) =>
     theme.unstable_sx({
         height: 20,
-
         color: '#fff',
         background: 'none',
 
@@ -16,6 +15,15 @@ export const StyledButton = styled(Button)(({ theme }) =>
 
         '&.Mui-focusVisible': {
             outline: `2px solid ${theme.palette.background.focusOutline}`
+        },
+
+        '&.active': {
+          borderRadius: '60px',
+
+          boxShadow:
+                'inset 0px 0px 4px 10px #6238c7, 0px 0px 4px 10px #6238c7;',
+
+          transition: 'linear 0.15s'
         }
     })
 );
