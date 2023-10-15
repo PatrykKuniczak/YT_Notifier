81ac186bf9aad4367f378e15d71e6bec8de28486
Patryk Kuniczak <p.kuniczak@gmail.com>
2023-10-15 18:56:08 +0200

    fix: resolve conflicts

--- react-app/utils/libs/query-client/index.ts
+++ react-app/utils/libs/query-client/index.ts
@@ -1,6 +1,6 @@
 import { QueryCache, QueryClient } from '@tanstack/react-query';
 
-export { useQuery, QueryClientProvider } from '@tanstack/react-query';
+export { QueryClientProvider, useQuery, useMutation } from '@tanstack/react-query';
 
 // TODO: DODAC TOASTIFY
 export default new QueryClient({
