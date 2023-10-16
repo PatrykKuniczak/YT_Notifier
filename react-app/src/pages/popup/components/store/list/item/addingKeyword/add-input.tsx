eab7a6bb806f5c37552621119e4bf25559c6602b
Bartosz Wysowski <117449119+TheMadNerd@users.noreply.github.com>
2023-10-16 21:56:39 +0200

    fix(add-input.tsx): remove-StyleErrorMessage (#115)

--- react-app/src/pages/popup/components/store/list/item/addingKeyword/add-input.tsx
+++ react-app/src/pages/popup/components/store/list/item/addingKeyword/add-input.tsx
@@ -2,7 +2,6 @@ import plusIcon from '@assets/img/plus-icon.svg';
 import { FormControl } from '@mui/base';
 import { styled } from '@mui/system';
 import { StyledButton } from '@pages/popup/components/shared/button';
-import { StyledErrorMessage } from '@pages/popup/components/shared/error';
 import { StyledIcon } from '@pages/popup/components/shared/icon';
 import StyledInput from '@pages/popup/components/shared/input';
 
@@ -59,7 +58,6 @@ const StyledAddInput = () => {
   return (
     <StyledForm>
       <FormControl defaultValue="" required style={{ width: '100%', position: 'relative' }}>
-        <StyledErrorMessage />
         <StyledKeywordInput placeholder="Dodaj słowo kluczowe" />
       </FormControl>
       <StyledSubmitButton type={'submit'}>
