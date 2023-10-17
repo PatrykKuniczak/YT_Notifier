03c33926a7efc8109800db134e10ef3814cfcd0e
Bartosz Wysowski <bartoszwysowski@gmail.com>
2023-10-17 14:47:38 +0200

    fix: mistake  with max validationNumber

--- react-app/utils/hooks/use-validate.ts
+++ react-app/utils/hooks/use-validate.ts
@@ -9,7 +9,7 @@ const useValidate = (): {
   const handleValidation = (props: React.ChangeEvent<HTMLInputElement>) => {
     const validationNumber = props.target.value.length;
 
-    if (validationNumber < 3 || validationNumber > 6) {
+    if (validationNumber < 3 || validationNumber > 255) {
       setDisable(true);
     } else {
       setDisable(false);
