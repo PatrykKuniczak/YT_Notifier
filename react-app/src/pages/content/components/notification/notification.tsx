e9c9d0081a75eb2542271a6a763dbb87b6efb023
Patryk Kuniczak <p.kuniczak@gmail.com>
2023-10-18 22:07:30 +0200

    fix: display properly close X button

--- react-app/src/pages/content/components/notification/notification.tsx
+++ react-app/src/pages/content/components/notification/notification.tsx
@@ -36,7 +36,7 @@ const Notification = ({ open, toggleOpen, content }: { open: boolean; toggleOpen
         <Portal>
           <StyledNotification>
             <StyledNotificationButton onClick={toggleOpen} aria-label="closing notification">
-              <StyledIcon src={xIcon} alt={'close'} width={20} height={20} />
+              <StyledIcon src={chrome.runtime.getURL(xIcon)} alt={'close'} width={20} height={20} />
             </StyledNotificationButton>
             <StyledNotificationContent>{content}</StyledNotificationContent>
           </StyledNotification>
