db3eefe0a9ccc44884c878264c91cd5c442b3c42
masterHAWK99 <maciej.pochron99@gmail.com>
2023-10-21 21:11:14 +0200

    feat: add option to context menu for adding new keyword

--- react-app/src/pages/background/index.ts
+++ react-app/src/pages/background/index.ts
@@ -1,3 +1,35 @@
+import urls from '@utils/endpoints/urls';
 import reloadOnUpdate from 'virtual:reload-on-update-in-background-script';
 
 reloadOnUpdate('pages/background');
+
+const createProperties = {
+  id: 'acae3286',
+  title: `Subscribe '%s' keyword`,
+  contexts: ['selection'],
+};
+
+chrome.contextMenus.create(createProperties, () => console.log('Context menu created!'));
+
+chrome.contextMenus.onClicked.addListener(({ selectionText }) => {
+  if (selectionText.length > 255) {
+    //TODO: show error message
+    return;
+  }
+
+  fetch(`${import.meta.env.VITE_API_URL}${urls.keywords}`, {
+    method: 'POST',
+    credentials: 'include',
+    body: JSON.stringify({ content: selectionText }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(r => r.json())
+    .then(r => console.log(r))
+    .catch(err => console.log(err));
+
+  //TODO: show success or error message
+
+  console.log('Context menu clicked!', selectionText);
+});
