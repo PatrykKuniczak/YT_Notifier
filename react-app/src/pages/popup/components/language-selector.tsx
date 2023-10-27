6c96d44a55e722f40c2a4985a935dea3672d07f4
masterHAWK99 <maciej.pochron99@gmail.com>
2023-10-27 22:05:47 +0200

    feat: use language from browser as default

--- react-app/src/pages/popup/components/language-selector.tsx
+++ react-app/src/pages/popup/components/language-selector.tsx
@@ -8,8 +8,9 @@ import { StyledButton } from '@pages/popup/components/shared/button';
 import { StyledListbox, StyledMenuButton, StyledMenuItem } from '@pages/popup/components/shared/dropdown';
 import { StyledIcon } from '@pages/popup/components/shared/icon';
 import i18n, { changeLanguage } from 'i18next';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useLocalStorage } from 'usehooks-ts';
 
 const languages = {
   en: { icon: { img: englishFlag, alt: 'English flag' }, nativeName: 'English' },
@@ -26,6 +27,19 @@ const LanguageSelector = () => {
   const theme = useTheme();
 
   const { t } = useTranslation();
+
+  const navigatorLanguage = new Intl.Locale(navigator.language).minimize().baseName;
+
+  const [language, setLanguage] = useLocalStorage('language', navigatorLanguage === 'pl' ? navigatorLanguage : 'en');
+
+  useEffect(() => {
+    changeLanguage(language);
+  }, [language]);
+
+  const handleClick = (language: string) => {
+    changeLanguage(language);
+    setLanguage(language);
+  };
 
   return (
     <Dropdown onOpenChange={handleOpenChange}>
@@ -51,7 +65,7 @@ const LanguageSelector = () => {
             <StyledButton
               sx={{ display: 'flex', alignItems: 'center', gap: 1 }}
               type="submit"
-              onClick={() => changeLanguage(language)}>
+              onClick={() => handleClick(language)}>
               <StyledIcon src={languages[language].icon.img} alt={languages[language].icon.alt} height={16} />
               {t(`languages.${language}`)}
             </StyledButton>
