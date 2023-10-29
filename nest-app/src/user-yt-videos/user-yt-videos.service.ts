83c9c00ad2e2c00dbde19e74c3d0070348ef87e5
Patryk Kuniczak <p.kuniczak@gmail.com>
2023-10-29 19:30:36 +0100

    fix: adjust findAll from yt videos service for new relations

--- nest-app/src/user-yt-videos/user-yt-videos.service.ts
+++ nest-app/src/user-yt-videos/user-yt-videos.service.ts
@@ -1,5 +1,5 @@
 import { youtube, youtube_v3 } from '@googleapis/youtube';
-import { Inject, Injectable } from '@nestjs/common';
+import { ForbiddenException, Inject, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { OAuth2Client } from 'google-auth-library';
 import { Repository } from 'typeorm';
@@ -13,70 +13,81 @@ export class UserYtVideosService {
   constructor(
     @Inject(OAUTH2_GOOGLE_CLIENT) private readonly oAuth2GoogleClient: OAuth2Client,
     @InjectRepository(UserYtVideosEntity)
-    private readonly ytVideosRepository: Repository<UserYtVideosEntity>,
+    private readonly userYtVideosRepository: Repository<UserYtVideosEntity>,
   ) {
     this.youtubeClient = youtube({ version: 'v3', auth: this.oAuth2GoogleClient });
   }
 
   async findAll(userId: number) {
-    const user = await this.findOneByUserId(userId);
+    const {
+      user: {
+        keywords,
+        userYtVideos: { lastFetch },
+      },
+    } = await this.findOneByUserId(userId);
 
-    console.log(user);
+    if (keywords.length) {
+      try {
+        const keywordsQueryResult = await Promise.all(
+          keywords.map(({ content }) =>
+            this.youtubeClient.search.list({
+              part: ['snippet'],
+              maxResults: 10,
+              order: 'date',
+              publishedAfter: lastFetch.toISOString(),
+              q: content,
+              type: ['video'],
+            }),
+          ),
+        );
 
-    //   if (keywordsResult.length) {
-    //     try {
-    //       const keywords = await Promise.all(
-    //         keywordsResult.map(({ content }) =>
-    //           this.youtubeClient.search.list({
-    //             part: ['snippet'],
-    //             maxResults: 10,
-    //             order: 'date',
-    //             // publishedAfter: lastFetch.toISOString(),
-    //             q: content,
-    //             type: ['video'],
-    //           }),
-    //         ),
-    //       );
-    //
-    //       const rawObjectForResponse = await Promise.all(
-    //         keywords.map(async (keyword, index) => {
-    //           if (keyword.data.items.length) {
-    //             const channelId = keyword.data.items[index].snippet.channelId;
-    //             const channelResult = await this.youtubeClient.channels.list({ id: [channelId], part: ['snippet'] });
-    //
-    //             return {
-    //               video: {
-    //                 id: keyword.data.items[index].id.videoId,
-    //                 title: keyword.data.items[index].snippet.title,
-    //                 description: keyword.data.items[index].snippet.description,
-    //                 publishedAt: keyword.data.items[index].snippet.publishedAt,
-    //                 thumbnail: keyword.data.items[index].snippet.thumbnails.medium.url,
-    //               },
-    //               channel: {
-    //                 id: channelId,
-    //                 title: channelResult.data.items[0].snippet.title,
-    //                 thumbnail: channelResult.data.items[0].snippet.thumbnails.default.url,
-    //               },
-    //             };
-    //           }
-    //         }),
-    //       );
-    //
-    //       return rawObjectForResponse.filter(keyword => keyword);
-    //     } catch (err) {
-    //       if ((err.status === 403 && err.errors[0].domain === 'youtube.quota') || err.status === 429) {
-    //         throw new ForbiddenException('You reach the requests limit for youtube');
-    //       }
-    //
-    //       throw err;
-    //     }
-    //   }
+        const rawObjectForResponse = await Promise.all(
+          keywordsQueryResult.map(async (queryResultKeyword, index) => {
+            if (queryResultKeyword.data.items.length) {
+              const channelId = queryResultKeyword.data.items[index].snippet.channelId;
+              const channelResult = await this.youtubeClient.channels.list({ id: [channelId], part: ['snippet'] });
+
+              return {
+                video: {
+                  id: queryResultKeyword.data.items[index].id.videoId,
+                  title: queryResultKeyword.data.items[index].snippet.title,
+                  description: queryResultKeyword.data.items[index].snippet.description,
+                  publishedAt: queryResultKeyword.data.items[index].snippet.publishedAt,
+                  thumbnail: queryResultKeyword.data.items[index].snippet.thumbnails.medium.url,
+                },
+                channel: {
+                  id: channelId,
+                  title: channelResult.data.items[0].snippet.title,
+                  thumbnail: channelResult.data.items[0].snippet.thumbnails.default.url,
+                },
+              };
+            }
+          }),
+        );
+
+        await this.updateLastFetchDate(userId);
+
+        return rawObjectForResponse.filter(keyword => keyword);
+      } catch (err) {
+        if ((err.status === 403 && err.errors[0].domain === 'youtube.quota') || err.status === 429) {
+          throw new ForbiddenException('You reach the requests limit for youtube');
+        }
+
+        throw err;
+      }
+    }
   }
 
   async findOneByUserId(userId: number) {
-    return this.ytVideosRepository.findOne({
-      relations: { user: true },
+    return this.userYtVideosRepository.findOne({
+      relations: { user: { keywords: true, userYtVideos: true } },
       where: { user: { id: userId } },
+    });
+  }
+
+  async updateLastFetchDate(userId: number) {
+    return this.userYtVideosRepository.update({ user: { id: userId } }, { lastFetch: new Date() }).catch(err => {
+      throw new InternalServerErrorException('Error on updating fetch date of user: ' + err.message);
     });
   }
 
@@ -120,14 +131,6 @@ export class UserYtVideosService {
 
   // async updatePlaylistId(id: number, playlistId: string) {
   //   const { affected } = await this.userRepository.update({ id }, { playlistId });
-  //
-  //   return !!affected;
-  // }
-
-  // async updateLastFetchDate(userId: number) {
-  //   const { affected } = await this.userRepository.update({ id: userId }, { lastFetch: new Date() }).catch(err => {
-  //     throw new InternalServerErrorException('Error on updating fetch date of user: ' + err.message);
-  //   });
   //
   //   return !!affected;
   // }
