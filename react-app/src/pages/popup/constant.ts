36ab6773328beb57d75b4a39f6ff77fa3d70b523
Bartosz Wysowski <bartoszwysowski@gmail.com>
2023-11-07 10:38:16 +0100

    refactor: change params of auth popup

--- react-app/src/pages/popup/constant.ts
+++ react-app/src/pages/popup/constant.ts
@@ -1,5 +1,5 @@
-export const POPUP_WIDTH = 500;
-export const POPUP_HEIGHT = 500;
+export const POPUP_WIDTH = 600;
+export const POPUP_HEIGHT = 1000;
 
 export const POPUP_POSITION_LEFT = (screen.width - POPUP_WIDTH) / 2;
 export const POPUP_POSITION_TOP = (screen.height - POPUP_HEIGHT) / 2;
