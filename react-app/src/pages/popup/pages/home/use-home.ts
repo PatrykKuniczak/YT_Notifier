7d275e27814396b4b8f75931633a6fc4ce251c61
Bartosz Wysowski <bartoszwysowski@gmail.com>
2023-11-17 14:19:30 +0100

    fix: reverse order of condition for setTitle

--- react-app/src/pages/popup/pages/home/use-home.ts
+++ react-app/src/pages/popup/pages/home/use-home.ts
@@ -13,7 +13,7 @@ export const useHome = () => {
   const { t } = useTranslation();
 
   useEffect(() => {
-    setTitle(pathname === '/store' ? t('foundVideos') : t('savedKeywords'));
+    setTitle(pathname === '/store' ? t('savedKeywords') : t('foundVideos'));
   }, [pathname, t]);
 
   return { title, location, ref, focus };
