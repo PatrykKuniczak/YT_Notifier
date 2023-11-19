5b39ba00cff6021e62a8be5a6c69d523b519b138
Patryk Kuniczak <p.kuniczak@gmail.com>
2023-11-19 19:41:11 +0100

    fix: remove unnecessary import

--- nest-app/src/users/users.module.ts
+++ nest-app/src/users/users.module.ts
@@ -1,12 +1,11 @@
-import { forwardRef, Global, Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserYtVideosModule } from '../user-yt-videos/user-yt-videos.module';
 import { UsersEntity } from './model/users.entity';
 import { UsersService } from './users.service';
 
 @Global()
 @Module({
-  imports: [forwardRef(() => UserYtVideosModule), TypeOrmModule.forFeature([UsersEntity])],
+  imports: [TypeOrmModule.forFeature([UsersEntity])],
   providers: [UsersService],
   exports: [UsersService],
 })
