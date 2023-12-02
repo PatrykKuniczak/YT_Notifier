531e3815f2bc20b0642d22598ff0338559253bca
Patryk Kuniczak <p.kuniczak@gmail.com>
2023-12-02 17:52:53 +0100

    chore: create displaying videos and enable interactivity

--- react-app/src/pages/popup/components/video/section/article/videoInfo/video-infos.tsx
+++ react-app/src/pages/popup/components/video/section/article/videoInfo/video-infos.tsx
@@ -1,15 +1,20 @@
-import { IVideo } from '@interfaces';
+import { IUserVideo } from '@interfaces';
 import { useTranslation } from '@internationalization';
 import { Stack } from '@mui/system';
 import { StyledVideoInfo } from '@pages/popup/components/video/section/article/videoInfo/video-info';
 import { StyledVideoTitle } from '@pages/popup/components/video/section/article/videoInfo/video-title';
 
-export const StyledVideoInfos = ({ publishedAt, views, title }: Pick<IVideo, 'publishedAt' | 'views' | 'title'>) => {
+export const StyledVideoInfos = ({
+  publishedAt,
+  views,
+  title,
+  videoUrl,
+}: Pick<IUserVideo['video'], 'publishedAt' | 'title' | 'views'> & { videoUrl: string }) => {
   const { t } = useTranslation();
 
   return (
-    <Stack useFlexGap={true} spacing={1}>
-      <Stack direction={'row'} alignItems={'center'} justifyContent={'center'} useFlexGap={true} spacing={2}>
+    <Stack useFlexGap={true} spacing={1} onClick={() => window.open(videoUrl)}>
+      <Stack direction={'row'} alignItems={'center'} useFlexGap={true} spacing={2}>
         <StyledVideoInfo>{t('timeAgo', { date: new Date(publishedAt) })}</StyledVideoInfo>
         <StyledVideoInfo>
           {views} {t('views')}
--- react-app/src/pages/popup/routes/videos.route.tsx
+++ react-app/src/pages/popup/routes/videos.route.tsx
@@ -1,47 +1,29 @@
-import thumbnail from '@assets/img/thumbnail.png';
 import useSearch from '@hooks/use-search';
+import httpClient from '@http-client';
+import { IUserVideo } from '@interfaces';
 import { StyledVideoArticle } from '@pages/popup/components/video/section/article/video-article';
 import { StyledVideoArticleSkeleton } from '@pages/popup/components/video/section/skeleton/video-article-skeleton';
 import { StyledVideosSection } from '@pages/popup/components/video/section/videos-section';
+import { useQuery } from '@query-client';
+import urls from '@utils/endpoints/urls';
 import { useDeferredValue, useMemo } from 'react';
 
 export const VideosRoute = () => {
-  const videos = useMemo(
-    () => [
-      {
-        id: '1',
-        thumbnail: thumbnail,
-        avatar: thumbnail,
-        authorName: 'XYZ Franko',
-        publishedAt: '2023-10-30T19:21:20Z',
-        views: '1 mld',
-        title: 'Hodujemy gatunek, który będzie dominował nad nami Hodujemy gatunek, który będzie dominował nad nami',
-      },
-      {
-        id: '2',
-        thumbnail: thumbnail,
-        avatar: thumbnail,
-        authorName: 'Test Testowski',
-        publishedAt: '2021-08-10T16:41:20Z',
-        views: '10 mln',
-        title: 'To jest Test To jest Test To jest Test To jest Test To jest Test',
-      },
-    ],
-    [],
-  );
-
-  const isLoading = false;
+  const { data: videos, isLoading: videosIsLoading } = useQuery<IUserVideo[]>({
+    queryKey: [urls.ytVideos.getVideos],
+    queryFn: () => httpClient.get(urls.ytVideos.getVideos).then(({ data }) => data),
+  });
 
   const { searchParamValue } = useSearch();
 
   const deferredSearchParam = useDeferredValue(searchParamValue);
 
   const filteredVideos = useMemo(
-    () => videos?.filter(({ title }) => title.includes(deferredSearchParam)),
+    () => videos?.filter(({ video: { title } }) => title.includes(deferredSearchParam)),
     [deferredSearchParam, videos],
   );
 
-  if (isLoading) {
+  if (videosIsLoading) {
     return (
       <StyledVideosSection>
         <StyledVideoArticleSkeleton />
@@ -51,7 +33,7 @@ export const VideosRoute = () => {
   } else {
     return (
       <StyledVideosSection>
-        {filteredVideos?.map(({ id, ...restProps }) => <StyledVideoArticle key={id} {...restProps} />)}
+        {filteredVideos?.map(userVideo => <StyledVideoArticle key={userVideo.video.id} {...userVideo} />)}
       </StyledVideosSection>
     );
   }
