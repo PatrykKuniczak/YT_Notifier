d96201f2836ff9d514cdd9827d2739141b5e7226
masterHAWK99 <maciej.pochron99@gmail.com>
2023-12-08 20:00:01 +0100

    improvement: add proper tag for click events

--- react-app/src/pages/popup/components/video/section/article/videoInfo/video-infos.tsx
+++ react-app/src/pages/popup/components/video/section/article/videoInfo/video-infos.tsx
@@ -5,6 +5,7 @@ import { useTranslation } from '@internationalization';
 import { Stack } from '@mui/system';
 import { StyledButton } from '@pages/popup/components/shared/button';
 import { StyledIcon } from '@pages/popup/components/shared/icon';
+import { StyledVideoButton } from '@pages/popup/components/video/section/article/video-button';
 import { StyledVideoInfo } from '@pages/popup/components/video/section/article/videoInfo/video-info';
 import { StyledVideoTitle } from '@pages/popup/components/video/section/article/videoInfo/video-title';
 import queryClient, { useMutation } from '@query-client';
@@ -40,16 +41,19 @@ export const StyledVideoInfos = ({
   return (
     <Stack useFlexGap={true} spacing={1}>
       <Stack direction={'row'} alignItems={'center'} useFlexGap={true} spacing={2}>
-        <StyledVideoInfo onClick={() => window.open(videoUrl)}>
-          {t('timeAgo', { date: new Date(publishedAt) })}
-        </StyledVideoInfo>
-        <StyledVideoInfo onClick={() => window.open(videoUrl)}>{t('views', { amount: views })}</StyledVideoInfo>
+        <StyledVideoButton onClick={() => window.open(videoUrl)}>
+          <StyledVideoInfo onClick={() => window.open(videoUrl)}>
+            {t('timeAgo', { date: new Date(publishedAt) })}
+          </StyledVideoInfo>
+          <StyledVideoInfo>{t('views', { amount: views })}</StyledVideoInfo>
+        </StyledVideoButton>
         <StyledButton title={t('nav.watchLater')} aria-label={t('nav.watchLater')} onClick={() => updatePlaylist()}>
           <StyledIcon src={watchLaterIcon} alt={''} width={14} height={14} />
         </StyledButton>
       </Stack>
-
-      <StyledVideoTitle>{title}</StyledVideoTitle>
+      <StyledVideoButton onClick={() => window.open(videoUrl)}>
+        <StyledVideoTitle>{title}</StyledVideoTitle>
+      </StyledVideoButton>
     </Stack>
   );
 };
