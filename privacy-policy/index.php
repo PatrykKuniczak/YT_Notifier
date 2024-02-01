55db48251a991af010860b21f152b4ea2f73822c
Alan1234111 <97180632+Alan1234111@users.noreply.github.com>
2024-02-01 17:26:48 +0100

    feat(privacy-policy): create styles and split html into smaller parts (#228)
    * feat(privacy-policy): create styles and split html into smaller parts
    
    * fix(privacy-policy): Delete unuse vars, add hover events to links
    
    * fix(privacy-policy): add class to nav li

---
+++ privacy-policy/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pl">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Polityka Prywatności - YT Notifier</title>
+  </head>
+
+  <body>
+    <nav>
+      <h1>Language:</h1>
+      <ol>
+        <li>
+          <a href="#polish-policy">Polski</a>
+          <ol>
+            <li class="nav-list-item"><a href="#polish-general-info">Informacje ogólne</a></li>
+            <li class="nav-list-item"><a href="#polish-protection-methods">Wybrane metody ochrony danych stosowane przez Operatora</a></li>
+            <li class="nav-list-item"><a href="#polish-hosting">Hosting</a></li>
+            <li class="nav-list-item"><a href="#polish-rights">Twoje prawa i dodatkowe informacje o sposobie wykorzystania danych </a></li>
+            <li class="nav-list-item"><a href="#polish-form-info">Informacje w formularzach</a></li>
+            <li class="nav-list-item"><a href="#polish-admin-logs">Logi Administratora</a></li>
+            <li class="nav-list-item"><a href="#polish-cookies">Informacja o plikach cookies</a></li>
+            <li class="nav-list-item"><a href="#polish-managing-cookies">Zarządzanie plikami cookies – jak w praktyce wyrażać i cofać zgodę? </a></li>
+          </ol>
+        </li>
+
+        <li>
+          <a href="#english-policy">English</a>
+          <ol>
+            <li class="nav-list-item"><a href="#english-general-info">General Information</a></li>
+            <li class="nav-list-item"><a href="#english-protection-methods">Selected data protection methods used by the Operator</a></li>
+            <li class="nav-list-item"><a href="#english-hosting">Hosting</a></li>
+            <li class="nav-list-item"><a href="#english-rights">Your rights and additional information about how your data is used </a></li>
+            <li class="nav-list-item"><a href="#english-form-info">Information in forms</a></li>
+            <li class="nav-list-item"><a href="#english-admin-logs">Administrator Logs</a></li>
+            <li class="nav-list-item"><a href="#english-cookies">Information about cookies </a></li>
+            <li class="nav-list-item"><a href="#english-managing-cookies">Managing cookies – how to express and withdraw consent in practice? </a></li>
+          </ol>
+        </li>
+      </ol>
+    </nav>
+
+
+    <?php 
+      include 'polish-policy.html';
+      include 'english-policy.html';
+    ?>
+  
+  </body>
+</html>
