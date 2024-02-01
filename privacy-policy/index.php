56cb4ff11d15020ae7350d4f3fbad24745460b59
Alan Pasoń <alan.pason132@gmail.com>
2024-02-01 17:22:37 +0100

    fix(privacy-policy): add class to nav li

--- privacy-policy/index.php
+++ privacy-policy/index.php
@@ -13,28 +13,28 @@
         <li>
           <a href="#polish-policy">Polski</a>
           <ol>
-            <li><a href="#polish-general-info">Informacje ogólne</a></li>
-            <li><a href="#polish-protection-methods">Wybrane metody ochrony danych stosowane przez Operatora</a></li>
-            <li><a href="#polish-hosting">Hosting</a></li>
-            <li><a href="#polish-rights">Twoje prawa i dodatkowe informacje o sposobie wykorzystania danych </a></li>
-            <li><a href="#polish-form-info">Informacje w formularzach</a></li>
-            <li><a href="#polish-admin-logs">Logi Administratora</a></li>
-            <li><a href="#polish-cookies">Informacja o plikach cookies</a></li>
-            <li><a href="#polish-managing-cookies">Zarządzanie plikami cookies – jak w praktyce wyrażać i cofać zgodę? </a></li>
+            <li class="nav-list-item"><a href="#polish-general-info">Informacje ogólne</a></li>
+            <li class="nav-list-item"><a href="#polish-protection-methods">Wybrane metody ochrony danych stosowane przez Operatora</a></li>
+            <li class="nav-list-item"><a href="#polish-hosting">Hosting</a></li>
+            <li class="nav-list-item"><a href="#polish-rights">Twoje prawa i dodatkowe informacje o sposobie wykorzystania danych </a></li>
+            <li class="nav-list-item"><a href="#polish-form-info">Informacje w formularzach</a></li>
+            <li class="nav-list-item"><a href="#polish-admin-logs">Logi Administratora</a></li>
+            <li class="nav-list-item"><a href="#polish-cookies">Informacja o plikach cookies</a></li>
+            <li class="nav-list-item"><a href="#polish-managing-cookies">Zarządzanie plikami cookies – jak w praktyce wyrażać i cofać zgodę? </a></li>
           </ol>
         </li>
 
         <li>
           <a href="#english-policy">English</a>
           <ol>
-            <li><a href="#english-general-info">General Information</a></li>
-            <li><a href="#english-protection-methods">Selected data protection methods used by the Operator</a></li>
-            <li><a href="#english-hosting">Hosting</a></li>
-            <li><a href="#english-rights">Your rights and additional information about how your data is used </a></li>
-            <li><a href="#english-form-info">Information in forms</a></li>
-            <li><a href="#english-admin-logs">Administrator Logs</a></li>
-            <li><a href="#english-cookies">Information about cookies </a></li>
-            <li><a href="#english-managing-cookies">Managing cookies – how to express and withdraw consent in practice? </a></li>
+            <li class="nav-list-item"><a href="#english-general-info">General Information</a></li>
+            <li class="nav-list-item"><a href="#english-protection-methods">Selected data protection methods used by the Operator</a></li>
+            <li class="nav-list-item"><a href="#english-hosting">Hosting</a></li>
+            <li class="nav-list-item"><a href="#english-rights">Your rights and additional information about how your data is used </a></li>
+            <li class="nav-list-item"><a href="#english-form-info">Information in forms</a></li>
+            <li class="nav-list-item"><a href="#english-admin-logs">Administrator Logs</a></li>
+            <li class="nav-list-item"><a href="#english-cookies">Information about cookies </a></li>
+            <li class="nav-list-item"><a href="#english-managing-cookies">Managing cookies – how to express and withdraw consent in practice? </a></li>
           </ol>
         </li>
       </ol>
